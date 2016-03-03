23dbeaf3c59ca73f5b53904d5029d2b922df46c0
Tyler Lutz <tlutz.computers@gmail.com>
2016-03-02 23:44:02 -0500

    Completed Client. Now Testing will begin

--- ASPMVCServerMonitorClient/Program.cs
+++ ASPMVCServerMonitorClient/Program.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Threading;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Net;
+using System.Management;
 
 namespace ASPMVCServerMonitorClient
 {
@@ -48,7 +52,58 @@ namespace ASPMVCServerMonitorClient
 
         static void RunPollingThread(object data)
         {
+            DateTime lastPollTime = DateTime.MinValue;
 
+            Console.WriteLine("Started polling...");
+
+            while (_running)
+            {
+                if((DateTime.Now - lastPollTime).TotalMilliseconds >= 1000)
+                {
+                    double cpuTime;
+                    ulong memUsage, totalMemory;
+
+                    GetMetrics(out cpuTime, out memUsage, out totalMemory);
+
+                    var postData = new
+                    {
+                        MachineName = System.Environment.MachineName,
+                        Processor = cpuTime,
+                        MemUsage = memUsage,
+                        TotalMemory = totalMemory
+                    };
+
+                    var json = JsonConvert.SerializeObject(postData);
+
+                    var serverUrl = new Uri(ConfigurationManager.AppSettings["ServerUrl"]);
+
+                    var client = new WebClient();
+                    client.Headers.Add("Content-Type", "application/json");
+                    client.UploadString(serverUrl, json);
+
+                    lastPollTime = DateTime.Now;
+                } else
+                {
+                    Thread.Sleep(10);
+                }
+            }
+        }
+
+        static void GetMetrics(out double processorTime, out ulong memUsage, out ulong totalMemory)
+        {
+            processorTime = (double)_cpuCounter.NextValue();
+            memUsage = (ulong)_memUsageCounter.NextValue();
+            totalMemory = 0;
+
+            // Get total memory from WMI
+            ObjectQuery memQuery = new ObjectQuery("SELECT * FROM CIM_OperatingSystem");
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(memQuery);
+
+            foreach (ManagementObject item in searcher.Get())
+            {
+                totalMemory = (ulong)item["TotalVisibleMemorySize"];
+            }
         }
     }
 }
